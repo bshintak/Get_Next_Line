2ad1047c5149fcc09c165416c1707de95bf8b23b
bshintak <87424758+bshintak@users.noreply.github.com>
2022-02-19 11:49:49 +0000

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -15,10 +15,15 @@
 size_t	ft_strlen(const char *str)
 {
 	size_t	i;
+	char	*cp;
 
 	i = 0;
-	while (str[i])
+	cp = (char *)(str);
+	while (cp && *cp)
+	{
+		cp++;
 		i++;
+	}
 	return (i);
 }
 
@@ -26,57 +31,80 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t	i;
 
+	if (!src && !dst)
+		return (0);
 	i = 0;
-	if (dest == src || n == 0)
-		return (dest);
 	while (i < n)
 	{
-		((char *)dest)[i] = ((const char *)src)[i];
+		((char *)dst)[i] = ((const char *)src)[i];
 		i++;
 	}
-	return (dest);
+	return (dst);
 }
 
 char	*ft_strjoin(const char *s1, const char *s2)
 {
+	size_t	i;
+	size_t	s1len;
+	size_t	s2len;
 	char	*ptr;
-	int		i;
-	int		j;
 
-	if (!s1 || !s2)
-		return (0);
-	ptr = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	i = 0;
+	ptr = NULL;
+	if (!s1 && !s2)
+		return (NULL);
+	if (s1 && !s2)
+		return (ft_strdup(s1));
+	else if (!s1 && s2)
+		return (ft_strdup(s2));
+	s1len = ft_strlen(s1);
+	s2len = ft_strlen(s2);
+	ptr = (char *)malloc(sizeof(char) * (s1len + s2len + 1));
 	if (!ptr)
-		return (0);
-	i = 0;
-	j = 0;
-	while (s1[i])
-		ptr[j++] = s1[i++];
-	i = 0;
-	while (s2[i])
-		ptr[j++] = s2[i++];
-	ptr[j] = '\0';
+		return (NULL);
+	while (i < (s1len + s2len + 1))
+		ptr[i++] = 0;
+	ft_memcpy(ptr, s1, s1len);
+	ft_memcpy(ptr + s1len, s2, s2len);
+	ptr[s1len + s2len] = '\0';
 	return (ptr);
 }
 
 char	*ft_strdup(const char *s)
 {
-	char	*dst;
-	size_t	i;
+	char	*str;
+	int		len;
 
-	i = ft_strlen(s) + 1;
-	dst = malloc(sizeof(char) * i);
-	if (!dst)
-		return (NULL);
-	ft_memcpy(dst, s, i);
-	return (dst);
+	if (s)
+	{
+		len = ft_strlen(s);
+		if (!len)
+			return (NULL);
+		str = (char *)malloc(sizeof(char) * (len + 1));
+		if (!str)
+			return (NULL);
+		ft_memcpy(str, s, len);
+		*(str + len) = '\0';
+		return (str);
+	}
+	return (NULL);
 }
 
 char	*ft_strchr(const char *s, int c)
 {
-	while (*s != '\0' && (unsigned char)c != *s)
-		s++;
-	if ((unsigned char)c == *s)
-		return ((char *)s);
-	return (0);
+	char	*tmp;
+
+	if (s)
+	{
+		tmp = (char *)(s);
+		while (*tmp)
+		{
+			if (*tmp == (char)c)
+				return (tmp);
+			tmp++;
+		}
+		if (*tmp == c)
+			return (tmp);
+	}
+	return (NULL);
 }
